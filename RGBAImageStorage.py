076abc8c784c912719569560bb299b2c40a36f15
Julien <tadelsucht@gmail.com>
2016-12-31 10:03:14 +0100

    RGB and RGBA

---
+++ RGBAImageStorage.py
@@ -0,0 +1,48 @@
+import os
+from time import sleep
+
+import array
+
+from PIL import Image
+from bitarray import bitarray
+
+COLOR_CODE_NON_DATA = (256, 256, 256, 256) # Weiss
+COLOR_CODE_SIZE = 32
+MAX_IMAGE_WIDTH = 4928
+MAX_IMAGE_HEIGHT = 3264
+
+INPUT_FILE_PATH = "go1.7.4.windows-amd64.msi"
+
+size = os.path.getsize(INPUT_FILE_PATH) * 8
+if size / COLOR_CODE_SIZE > MAX_IMAGE_WIDTH * MAX_IMAGE_HEIGHT:
+    raise Exception("File to big for one image")
+file = open(INPUT_FILE_PATH, 'rb')
+progress_counter = 0
+
+image = Image.new('RGBA', (MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT), COLOR_CODE_NON_DATA)
+pixels = image.load()
+
+
+x = 0
+y = 0
+bit_counter = 0
+bits = []
+
+ba = bitarray()
+ba.fromfile(file)
+for bit in ba:
+    bits.append(bit)
+    if len(bits) == COLOR_CODE_SIZE:
+        progress_counter += COLOR_CODE_SIZE
+        if progress_counter % (COLOR_CODE_SIZE*65536) == 0:
+            print("{0}".format(1.0*progress_counter / size * 64))
+        number = int(''.join('1' if i else '0' for i in bits), 2)
+        rgb = hex(number)[2:].zfill(8)
+        pixels[x, y] = (int(rgb[6:8], 16), int(rgb[4:6], 16), int(rgb[2:4],16), int(rgb[0:2],16))
+        bits = []
+        x += 1
+        if x == MAX_IMAGE_WIDTH:
+            y += 1
+            x = 0
+image.save('image.png')
+print("Finished")
