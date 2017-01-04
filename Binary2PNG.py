47255ded6628a78cfa88e1001f45db8ff1480906
Julien <tadelsucht@gmail.com>
2017-01-04 13:49:35 +0100

    Performance

--- Binary2PNG.py
+++ Binary2PNG.py
@@ -1,4 +1,7 @@
 import os
+import struct
+from time import sleep
+
 import png
 from bitarray import bitarray
 
@@ -24,26 +27,39 @@ bit_counter = 0
 bits = []
 image_counter = 0
 
+def get_int_value(data, index):
+    if index >= len(data):
+        return 0
+    else:
+        return struct.unpack('B', data[index + 0])[0]
+
+def get_rgba(data, index):
+    red = get_int_value(data, index + 0)
+    green = get_int_value(data, index + 1)
+    blue = get_int_value(data, index + 2)
+    alpha = get_int_value(data, index + 3)
+    return (red, green, blue, alpha)
+
 print("Start processing")
-ba = bitarray()
-ba.fromfile(file)
 rgba_array = list()
 row_array = list()
 
-for bit in ba:
-    bits.append(bit)
-    if len(bits) == COLOR_CODE_SIZE:
+BLOCK_SIZE = 2048
+byte_counter = 0
+file = open(INPUT_FILE_PATH, 'rb')
+while True:
+    data = file.read(BLOCK_SIZE)
+    if not data:
+        break
+
+    byte_counter = 0
+    while BLOCK_SIZE > byte_counter:
+        row_array.append(get_rgba(data, byte_counter))
+        byte_counter += COLOR_CODE_SIZE / 8
+
         progress_counter += COLOR_CODE_SIZE
-        if progress_counter % (COLOR_CODE_SIZE*65536) == 0:
-            print("{0}%".format(1.0*progress_counter / size * 100))
-        number = int(''.join('1' if i else '0' for i in bits), 2)
-        rgb = hex(number)[2:].zfill(8)
-        try:
-            pixel = (int(rgb[6:8], 16), int(rgb[4:6], 16), int(rgb[2:4],16), int(rgb[0:2],16))
-            row_array.append(pixel)
-        except:
-            raise Exception("{0}|{1}".format(x, y))
-        bits = []
+        if progress_counter % (COLOR_CODE_SIZE * 65536) == 0:
+            print("{0}%".format(1.0 * progress_counter / size * 100))
         x += 1
         if x == MAX_IMAGE_WIDTH:
             rgba_array.append(row_array)
@@ -53,11 +69,17 @@ for bit in ba:
         if y == MAX_IMAGE_HEIGHT:
             image = png.from_array(rgba_array, 'RGBA')
             image.save((INPUT_FILE_PATH + ".{0}.png").format(image_counter))
-            rgba_array  = list()
+            rgba_array = list()
             row_array = list()
             x = 0
             y = 0
             image_counter += 1
+
+i = len(row_array)
+while i < MAX_IMAGE_WIDTH:
+    i += 1
+    row_array.append((0,0,0,0))
+
 rgba_array.append(row_array)
 image = png.from_array(rgba_array, 'RGBA')
 image.save((INPUT_FILE_PATH + ".{0}.png").format(image_counter))
